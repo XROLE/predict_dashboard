0288a35c88b0d8a5aa9a90260018436b1ad0728b
xrole <xrolediamond@gmail.com>
2024-05-16 00:57:02 +0100

    create mobile body section

--- lib/views/dashboard/body/desktop_body.dart
+++ lib/views/dashboard/body/desktop_body.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:predict_dashboard/utils/app_colors.dart';
 import 'package:predict_dashboard/utils/app_text_style.dart';
 import 'package:predict_dashboard/views/dashboard/body/widget/desktop_row.dart';
+import 'package:predict_dashboard/views/dashboard/body/widget/mobile_row.dart';
 import 'package:predict_dashboard/views/dashboard/footer/footer.dart';
+import 'package:predict_dashboard/views/widgets/responsive_layout_builder.dart';
 
 class DesktopBody extends StatelessWidget {
   final Function() onFilter;
@@ -48,19 +50,34 @@ class DesktopBody extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 20),
-          Table(
-            children: [
-              DesktopTableRow.defaultRow(),
-              ...List.generate(5, (index) {
-                return DesktopTableRow.row(
+          ResponsiveLayoutBuilder(
+            mobileView: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const MobileRow(
                     symbol: "MINAUSDT",
                     price: "1.5636",
-                    type: "LMT",
                     action: "Sell",
-                    quantity: "0",
-                    date: "19 Dec, 2023");
-              }),
-            ],
+                    date: "19 Dec, 2023",
+                    hasBorderTop: true,
+                  );
+                }),
+            desktopView: Table(
+              children: [
+                DesktopTableRow.defaultRow(),
+                ...List.generate(5, (index) {
+                  return DesktopTableRow.row(
+                      symbol: "MINAUSDT",
+                      price: "1.5636",
+                      type: "LMT",
+                      action: "Sell",
+                      quantity: "0",
+                      date: "19 Dec, 2023");
+                }),
+              ],
+            ),
           ),
           const SizedBox(height: 20),
           const Footer(
